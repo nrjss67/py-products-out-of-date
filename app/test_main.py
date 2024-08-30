4d459718ffb026b1ca986754522545a2477bb701
Yaroslav Ivanov <extazzzyprofit@gmail.com>
2024-08-30 13:48:09 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,37 @@
-# write your code here
+import pytest
+from datetime import date
+from unittest import mock
+from app.main import outdated_products
+
+
+@pytest.fixture()
+def products() -> dict:
+    return [
+        {
+            "name": "salmon",
+            "expiration_date": date(2022, 2, 10),
+            "price": 600
+        },
+        {
+            "name": "chicken",
+            "expiration_date": date(2022, 2, 5),
+            "price": 120
+        },
+        {
+            "name": "duck",
+            "expiration_date": date(2022, 2, 1),
+            "price": 160
+        }
+    ]
+
+
+@pytest.mark.parametrize(
+    "expiration_date,expected",
+    [
+        (date(2022, 2, 9), ["chicken", "duck"])
+    ]
+)
+@mock.patch("app.main.datetime.date")
+def test_with_expired_date(mocked_today, expiration_date, expected, products): # noqa
+    mocked_today.today.return_value = expiration_date
+    assert outdated_products(products) == expected
